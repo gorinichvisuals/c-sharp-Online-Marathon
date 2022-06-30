7e61e71dbd6835f30ff988e0e610e325e1e28f5a
Egor <104780776+gorinichvisuals@users.noreply.github.com>
2022-06-30 22:27:23 +0300

    Create degeltask5.cs

---
+++ sprint3/degeltask5.cs
@@ -0,0 +1,36 @@
+public delegate void EventHandler();
+
+class EventProgram 
+{
+    public event EventHandler Show;
+
+    public EventProgram()
+    {        
+        Show += Dog;
+        Show += Cat;
+        Show += Mouse;
+        Show += Elephant;
+        
+        Show.Invoke();
+    }
+    
+    public static void Dog()
+    {
+        Console.WriteLine("Dog");
+    }
+
+    public static void Cat()
+    {
+        Console.WriteLine("Cat");
+    }
+
+    public static void Mouse()
+    {
+        Console.WriteLine("Mouse");
+    }
+
+    public static void Elephant()
+    {
+        Console.WriteLine("Elephant");
+    }
+} 
